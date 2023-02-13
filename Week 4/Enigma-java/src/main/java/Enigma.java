60b913898870d008d9c1c9370d229cb7a8206421
isabelconaghan <124704933+isabelconaghan@users.noreply.github.com>
2023-02-13 13:30:46 +0000

    reverse Caesar (1st draft)

--- Week 4/Enigma-java/src/main/java/Enigma.java
+++ Week 4/Enigma-java/src/main/java/Enigma.java
@@ -8,8 +8,18 @@ public final class Enigma {
 
     public static String Encrypt(String message, int incrementNumber,List<String> rotors ){
         // TODO - Implement the Encrypt method
+        String message = "Hello There";
+        int incrementNumber = 4;
+        List<String> rotors = new List<String>("abcdefghijklmnopqrstuvwxys");
+
         // Steps in brief
         // 1. Apply the CAESAR shift using the increment number
+        for(int i=0; i<rotors.length; i++)
+        {
+            rotors[i] = rotors.indexOf([i]+incrementNumber);
+            message = message*rotors[i];
+        }
+        System.out.println(message);
         // 2. For each rotor in the list rotors
         //  2.1 Translate the message using the rotor
         // 3. Return the encrypted string
@@ -21,9 +31,16 @@ public final class Enigma {
     public static String Decrypt(String message, int incrementNumber, List<String> rotors)
     {
         // TODO - Implement the Decrypt method
+        String message = "Hello There";
+        int incrementNumber = 4;
+        List<String> rotors = new List<String>("abcdefghijklmnopqrstuvwxys");
 
         // Steps in brief
         // 1. For each rotor in the list rotors, starting with the last rotor
+        for(int i=0; i<rotors.length-i; i++)
+        {
+
+        }
         //  1.1 Translate the message using the rotor
         // 2. Apply the CAESAR shift
         // 3. Return the decrypted string
